85352777a1548a26acd029d5905e4355ae46293c
Luciano Custodio <luciano@mundoslackware.com.br>
2024-03-13 23:27:49 -0300

    13-03

--- UC9/backend/src/Controller/Pedidos/PedidosController.ts
+++ UC9/backend/src/Controller/Pedidos/PedidosController.ts
@@ -20,34 +20,6 @@ class PedidosController {
         })
         return res.json(resposta)
     }
-
-    async criarItensPedido(req: Request, res: Response) {
-        const { quantidade, valor, id_pedido, id_produto } = req.body
-        const criarItensPedido = new PedidosServices()
-        const resposta = await criarItensPedido.criarItenspedido({
-            quantidade, valor, id_pedido, id_produto
-        })
-        return res.json(resposta)
-    }
-
-    async somarItensPedidos(req: Request, res: Response) {
-        const { id } = req.params
-        const somarItensPedido = new PedidosServices()
-        const resposta = await somarItensPedido.somarItensPedido({
-            id
-        })
-        return res.json(resposta)
-    }
-
-    async apagarItemPedido(req: Request, res: Response) {
-        const { id } = req.params
-        const apagarItemPedido = new PedidosServices()
-        const resposta = await apagarItemPedido.apagarItemPedido({
-            id
-        })
-        return res.json(resposta)
-    }
-
 }
 
 export { PedidosController }
